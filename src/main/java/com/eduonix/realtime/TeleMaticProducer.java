a8e61280d8e28e2b363d0d1e27809da03a0d6126
nsavagejvm <nscode1@gmail.com>
2015-08-04 21:47:43 +0300

    init

---
+++ src/main/java/com/eduonix/realtime/TeleMaticProducer.java
@@ -0,0 +1,53 @@
+package com.eduonix.realtime;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+import org.apache.log4j.Logger;
+
+import java.util.Properties;
+
+/**
+ * Created by ubu on 04.08.15.
+ */
+public class TeleMaticProducer {
+
+    private static final Logger LOG = Logger.getLogger(TeleMaticProducer.class);
+
+    private static final String brokerList= "sandbox.hortonworks.com:6667";
+    private static final String zookeeperHost= "sandbox.hortonworks.com:2181";
+
+    private static final String TOPIC = "realtime-event";
+
+    public static void main(String[] args) {
+
+
+        Properties props = new Properties();
+        props.put("metadata.broker.list", brokerList);
+        props.put("zk.connect", zookeeperHost);
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("request.required.acks", "1");
+
+        ProducerConfig config = new ProducerConfig(props);
+        Producer<String, String> producer = new Producer<String, String>(config);
+
+        String event = "";
+        int count = 0;
+
+        while(count< 5000) {
+
+            event= "stream event number "+ count++;
+            KeyedMessage<String, String> data = new KeyedMessage<String, String>(TOPIC, event);
+            LOG.info("Sending Messge #:msg:" + event);
+            producer.send(data);
+
+        }
+
+
+
+    }
+
+
+
+
+}
